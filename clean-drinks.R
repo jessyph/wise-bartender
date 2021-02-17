0035cee1d362db69cfd98e007340001a609c0fab
Jessy Parker Humphreys <Jessy@jessicas-air.home>
2021-02-17 15:19:10 +0000

    set up cleaning

--- clean-drinks.R
+++ clean-drinks.R
@@ -1,1 +1,14 @@
 library(tidyverse)
+
+d <- read.csv("~/Downloads/wise-bartender.csv")
+
+#Get list of products sold
+product <- d %>%
+  select(ProductName)
+
+#Select unique products sold
+unique <- product %>%
+  distinct(ProductName) 
+
+unique <- unique %>%
+  
